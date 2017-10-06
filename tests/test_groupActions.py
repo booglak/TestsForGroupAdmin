908878f2d2fbd4b3b7353d9ae950df87cd353bd4
I_Buglak <i.buglak@nullgr.com>
2017-10-06 19:00:39 +0300

    add test_createGroup

--- tests/test_groupActions.py
+++ tests/test_groupActions.py
@@ -9,12 +9,20 @@ class GroupActionsTest(unittest.TestCase):
     group = ga.GActions()
     group_id = 0
 
-    def test_createGroup(self):
+    def createGroup(self):
         create = self.group.create_group()
         self.group_id = create['data']['Value']
         groups = self.data.get_groups()
         string = "'Id': "+ str(self.group_id)
         assert string in str(groups['data']['Values'])
+
+    def test_updateGroup(self):
+        update = self.group.update_group(self.group_id)
+        groups = self.data.get_groups()
+        string = "'Limit': 20, 'Name': 'Группа обучения №15', 'Number': 15"
+        assert string in str(groups['data']['Values'])
+
+
 
 
 
